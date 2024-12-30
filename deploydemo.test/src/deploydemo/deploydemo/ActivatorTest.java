83550ec8a092587c8daed7fbeb3578da3eca7f0a
Ramsey Gurley <ramseygurley@gmail.com>
2024-12-30 14:18:49 +0900

    initial commit

---
+++ deploydemo.test/src/deploydemo/deploydemo/ActivatorTest.java
@@ -0,0 +1,13 @@
+package deploydemo.deploydemo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ActivatorTest {
+
+	@Test
+	public void testActivatorId() {
+		//Assertions.assertNotNull(Activator.PLUGIN_ID);
+	}
+
+}
